aa823ba1757d4d57f6151707955e2731b5d784d7
Aaron Porter and Zach Holz <aaron.w.porter@gmail.com>
2016-07-06 20:04:41 -0700

    Add running balance div and add deposit-withdrawl form

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
 // business logic
 
-function BankAccount(first, last, initial-deposit) {
+function BankAccount(first, last, initialDeposit) {
   this.firstName = first;
   this.lastName = last;
-  this.balance = initial-deposit;
+  this.balance = initialDeposit;
+}
+
+BankAccount.prototype.addDeposit = function(deposit) {
+  return this.balance += deposit;
 }
 
 // user interface logic
 $(document).ready(function() {
-  $("#create-account").click(function() {
+  $("form#create-account").submit(function(event) {
+    event.preventDefault();
 
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedFirstDeposit = parseInt($("input#initial-deposit").val());
+
+    var newAccount = new BankAccount(inputtedFirstName, inputtedLastName, inputtedFirstDeposit);
+
+    $("#running-balance").text(newAccount.balance);
+
+    // deposit or withdrawl
+
+    $("#deposit-withdrawl").click(function() {
+
+      var withdrawl = parseInt($("#withdrawl").val());
+      var deposit = parseInt($("#additional-deposit").val());
+
+      $("#running-balance").text(newAccount.addDeposit(deposit));
+    });
   });
+
 });
