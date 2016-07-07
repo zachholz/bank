2758850a995cfcbba0187f5a64c31d718991ce06
Aaron Porter and Zach Holz <zach.a.holz@gmail.com>
2016-07-06 18:48:54 -0700

    Add initial deposit form and js constructor

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// business logic
+
+function BankAccount(first, last, initial-deposit) {
+  this.firstName = first;
+  this.lastName = last;
+  this.balance = initial-deposit;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("#create-account").click(function() {
+
+  });
+});
